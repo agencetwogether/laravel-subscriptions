117490a20d38e73c13bfbc44cca6670c2f4831b4
agencetwogether <53862310+agencetwogether@users.noreply.github.com>
2023-09-13 21:40:59 +0200

    Update SubscriptionsServiceProvider.php

--- src/Providers/SubscriptionsServiceProvider.php
+++ src/Providers/SubscriptionsServiceProvider.php
@@ -24,9 +24,9 @@ class SubscriptionsServiceProvider extends ServiceProvider
      * @var array
      */
     protected $commands = [
-        MigrateCommand::class => 'command.rinvex.subscriptions.migrate',
-        PublishCommand::class => 'command.rinvex.subscriptions.publish',
-        RollbackCommand::class => 'command.rinvex.subscriptions.rollback',
+        MigrateCommand::class,
+        PublishCommand::class,
+        RollbackCommand::class,
     ];
 
     /**
@@ -58,8 +58,12 @@ class SubscriptionsServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // Publish Resources
-        $this->publishesConfig('rinvex/laravel-subscriptions');
-        $this->publishesMigrations('rinvex/laravel-subscriptions');
-        ! $this->autoloadMigrations('rinvex/laravel-subscriptions') || $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
+        $this->publishes([
+            __DIR__.'/../../config/config.php' => config_path('rinvex.subscriptions.php'),
+        ], 'rinvex/subscriptions::config');
+        $this->publishes([
+            __DIR__.'/../../database/migrations/' => database_path('migrations')
+        ], 'rinvex/subscriptions::migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
     }
 }
